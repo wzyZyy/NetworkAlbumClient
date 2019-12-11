69b317c51116bfe4c68eb0b66012f7b948a8c411
wangzhengyang <zy_wao@163.com>
2019-12-11 23:36:57 +0800

    LoginActivity

--- app/src/main/java/cn/zuel/wlyw/networkalbumclient/activity/LoginActivity.java
+++ app/src/main/java/cn/zuel/wlyw/networkalbumclient/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package cn.zuel.wlyw.networkalbumclient.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import cn.zuel.wlyw.networkalbumclient.R;
 import cn.zuel.wlyw.networkalbumclient.base.BaseActivity;
@@ -11,5 +13,12 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // 用户登录账号
+        EditText userAccountInput = (EditText) findViewById(R.id.login_input_user_account);
+        // 用户登录密码
+        EditText userPasswordInput = (EditText) findViewById(R.id.login_input_user_password);
+        // 登录按钮
+        Button loginBtn = (Button) findViewById(R.id.login_btn);
     }
 }
